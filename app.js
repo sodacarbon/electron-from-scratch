31829b7f5dab37295a779d74ccc7717785bdba45
sodacarbon <sodacarbon@gmail.com>
2016-07-31 23:14:42 +0700

    Read local file content

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ document.write('<br>The current version of io.js is ' +
   process.version);
 document.write('<br>The current Electron version is ' +
     process.versions.electron);
+
+// Read file
+var fs = require('fs');
+var contents = fs.readFileSync('./package.json', 'utf8')
+alert(contents);
