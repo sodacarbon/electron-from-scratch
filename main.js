2a0e1c08a8353d90a1c8ba453aeb09dd7a5ed8d6
sodacarbon <sodacarbon@gmail.com>
2016-07-31 23:02:40 +0700

    Electron: Create a window

---
+++ main.js
@@ -0,0 +1,9 @@
+const {app, BrowserWindow} = require('electron');
+
+app.on('ready', function () {
+  let mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: true,
+  });
+});
